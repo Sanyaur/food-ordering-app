558b5ceda0b25ee1d11de557f109e94829ca93fd
Sanyaur <gs.ujvari@gmail.com>
2022-04-05 20:47:32 +0200

    first commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Neat Foods, Neat Delivery</h2>
+      <p>Choose your favorite food from SandorMeals handmade food heaven </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
