443eca88569f322178ee9c6b9ab56aaaf55f32c7
Sanyaur <gs.ujvari@gmail.com>
2022-04-08 15:58:34 +0200

    cart state provider added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import { useState } from "react";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -12,13 +13,13 @@ function App() {
   const hideModalHandler = () => setIsModalVisible(false);
 
   return (
-    <>
+    <CartProvider>
       {isModalVisible && <Cart onHideCart={hideModalHandler} />}
       <Header onShowCart={showModalHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
