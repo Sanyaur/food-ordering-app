b2a87ea04100e8b4e7f6bd83f85b0a1547a54420
Sanyaur <gs.ujvari@gmail.com>
2022-04-11 16:37:22 +0200

    added useReducer hook with states

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { CartContext } from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = () => "";
+const initialValue = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = () => "";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "":
+      return {
+        // returns a brand new array which considered a better way of handling state (?)
+        items: state.items.concat(action.item),
+        // returns the previous amount, then adds the price as many times as the number of products in the basket.
+        totalAmount: state.totalAmount + action.item.price * action.item.amount,
+      };
+
+    default:
+      return initialValue;
+  }
+};
+const CartProvider = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialValue);
+
+  const addItemToCartHandler = (item) =>
+    dispatch({ type: "ADD", items: { id: item.id, item: item } });
+
+  const removeItemFromCartHandler = (id) =>
+    dispatch({ type: "REMOVE", id: id });
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: state.items,
+    totalAmount: state.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
