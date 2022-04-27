8d9841e53ae3fb45c4bc558434104b6feca7d6cb
Sanyaur <gs.ujvari@gmail.com>
2022-04-27 15:56:37 +0200

    same product amount increment feature implemented

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,11 +9,32 @@ const initialValue = {
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD":
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      const existingCartItem = state.items[existingCartItemIndex];
+
+      let updatedItems;
+
+      // checks if the product is already existing in the cart, and increment the amount of it
+      if (existingCartItem) {
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + action.payload.amount,
+        };
+      } else {
+        updatedItems = state.items.concat(action.payload);
+      }
+
       return {
-        // returns a brand new array which considered a better way of handling state (?)
-        items: state.items.concat(action.item),
+        ...state,
         // returns the previous amount, then adds the price as many times as the number of products in the basket.
-        totalAmount: state.totalAmount + action.item.price * action.item.amount,
+        totalAmount:
+          state.totalAmount + action.payload.price * action.payload.amount,
+        // returns a brand new array which considered a better way of handling state (?)
+        items: updatedItems,
       };
 
     default:
@@ -23,7 +44,8 @@ const reducer = (state, action) => {
 const CartProvider = (props) => {
   const [state, dispatch] = useReducer(reducer, initialValue);
 
-  const addItemToCartHandler = (item) => dispatch({ type: "ADD", item: item });
+  const addItemToCartHandler = (item) =>
+    dispatch({ type: "ADD", payload: item });
 
   const removeItemFromCartHandler = (id) =>
     dispatch({ type: "REMOVE", id: id });
