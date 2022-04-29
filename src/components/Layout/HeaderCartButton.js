50ac3aaaf6d897e4c0664c4ea5b18f53ecd3b999
Sanyaur <gs.ujvari@gmail.com>
2022-04-29 15:33:15 +0200

    conditional bumping animation added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import { CartContext } from "../../store/cart-context";
 
 export const HeaderCartButton = (props) => {
-  const { button, icon, badge } = classes;
+  const [buttonHighlight, setButtonHighlight] = useState(false);
 
+  const { button, icon, badge, bump } = classes;
   const { items } = useContext(CartContext);
 
   const totalItemNumber = items.reduce(
@@ -13,8 +14,22 @@ export const HeaderCartButton = (props) => {
     0
   );
 
+  const buttonClasses = `${button} ${buttonHighlight && bump}`;
+  useEffect(() => {
+    console.log(items.length);
+    items.length > 0 && setButtonHighlight(true);
+
+    const timer = setTimeout(() => {
+      setButtonHighlight(false);
+    }, 100);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={button} onClick={props.onShowCart}>
+    <button className={buttonClasses} onClick={props.onShowCart}>
       <span className={icon}>
         <CartIcon />
       </span>
